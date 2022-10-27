0065c17123bdb8a6e425913e3b562ff38fc5f8be
Wilfredo Luna <61670708+LunaWilfredo@users.noreply.github.com>
2022-10-27 14:31:03 -0500

    SSO
    Correcciones de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,48 +2,33 @@
 @section('content')
 {{-- Contenido --}}
 <div class="container p-5 mt-5">
-    {{-- comentado temporal para colocar imagen  --}}
-    {{-- <div class="row">
-        <div class="col text-center">
-            <h1 class="h1 display-1">
-                Bienvenido a tu sistema PYME
-            </h1>
-        </div>
-    </div> --}}
-    {{-- fin de comentao temporal --}}
-    <div class="row">
-        {{-- <div class="card-header">{{ __('Login') }}</div> --}}
-        <div class="col-8">
-            {{-- <img src="public/img/ping.png" alt="" class="img"> --}}
+    <div class="row p-5">
+        <div class="col-8 p-4">
             <h1 class="h1 display-1 text-primary">
-                Bienvenido a tu sistema PYME
+                Bienvenido a tu sistema <span class="fw-bold text-success">PYME</span>
             </h1>
         </div>
         <div class="col-4 p-5">
             <form method="POST" action="{{ route('login') }}">
             @csrf
-                <div class="row">
-                    <div class="mb-3">
+                <div class="row d-flex justify-content-center align-content-center m-auto">
+                    <div class="col-12 mb-3">
                         {{-- <label for="email" class="">{{ __('Email') }}</label> --}}
-                        <div class="">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail">
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
-                    <div class="mb-3">
+                    <div class="col-12 mb-3">
                         {{-- <label for="password" class="">{{ __('Contraseña') }}</label> --}}
-                        <div class="">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Contraseña">
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Contraseña">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
                     <div class="">
                         <button type="submit" class="btn btn-primary w-100">
